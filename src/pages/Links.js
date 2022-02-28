9a03152d7f44d52c2cf88fa9db334fb29d7932ca
James <jimibue@gmail.com>
2022-02-28 12:17:41 -0800

    crud in provider done

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { DataContext } from '../providers/DataProvider'
 
 const Links = ()=>{
+    //useContext always return object just destructor keys
+    
+    const { links, getLinks, createLink, updateLink ,deleteLink} = useContext(DataContext)
     return (
+        <div>
         <h1>Links here</h1>
+        <div>{JSON.stringify(links)}</div>
+        <button onClick={getLinks}>Get Links</button>
+        <button onClick={()=>createLink( {title:'title here', username:'jamesy'} )}>create link</button>
+        <button onClick={()=>updateLink( {id:19, title:'UPDATED'} )}>update link</button>
+        <button onClick={()=>deleteLink(19)}>delete link</button>
+        </div>
     )
 }
 
